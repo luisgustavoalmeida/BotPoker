20f1579409d9d4bd3e1a9558481448b492436134
Luís Gustavo de Almeida <luis.gustavo@engenharia.ufjf.br>
2024-02-18 18:24:22 -0300

    up

--- BotPK.bat
+++ BotPK.bat
@@ -10,22 +10,6 @@ start "" "move_janela.bat.ahk"
 echo Pressione qualquer tecla para iniciar...
 pause >nul
 
-echo Iniciando em:
-REM Pausa por 1 segundo
-timeout /t 1 /nobreak >nul
-echo 5
-REM Pausa por 1 segundo
-timeout /t 1 /nobreak >nul
-echo 4
-REM Pausa por 1 segundo
-timeout /t 1 /nobreak >nul
-echo 3
-REM Pausa por 1 segundo
-timeout /t 1 /nobreak >nul
-echo 2
-REM Pausa por 1 segundo
-timeout /t 1 /nobreak >nul
-echo 1
 REM Pausa por 1 segundo
 timeout /t 1 /nobreak >nul
 
