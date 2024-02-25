32e989a343d84a593f06e9133c26dddc1d43752b
Luís Gustavo de Almeida <luis.gustavo@engenharia.ufjf.br>
2024-02-25 15:21:27 -0300

    Modificaçoes para poder mudar a função de forma automatica. Sistema de notificação via telegran.

--- Telegran.py
+++ Telegran.py
@@ -7,12 +7,15 @@ from Google import numero_pc
 # Site para encontar os emojes https://emojipedia.org/pt   https://emojipedia.org/pt/google
 
 async def enviar_mensagem(mensagem, token='6446494638:AAE5lkb9s74m_5R8DE-z2uDqt5PrYEQUTo8', chat_id='-1001971264843', ):
-    # Inicializa o bot com o token fornecido
-    bot = Bot(token)
+    try:
+        # Inicializa o bot com o token fornecido
+        bot = Bot(token)
 
-    # Envia a mensagem para o chat especificado
-    await bot.send_message(chat_id=chat_id, text=mensagem, disable_notification=False)
-
+        # Envia a mensagem para o chat especificado
+        await bot.send_message(chat_id=chat_id, text=mensagem, disable_notification=False)
+    except Exception as e:
+        # Registra o erro e a mensagem
+        print(f"Erro ao enviar mensagem: {e}")
 
 def monta_mensagem(mensagem):
     mensagem_montada = '🤖  ' + numero_pc + ': ' + str(mensagem)
@@ -21,11 +24,16 @@ def monta_mensagem(mensagem):
 
 
 async def enviar_mensagem_silenciosa(mensagem, token='6446494638:AAE5lkb9s74m_5R8DE-z2uDqt5PrYEQUTo8', chat_id='-1001971264843', ):
-    # Inicializa o bot com o token fornecido
-    bot = Bot(token)
+    try:
+        # Inicializa o bot com o token fornecido
+        bot = Bot(token)
 
-    # Envia a mensagem para o chat especificado sem notificação
-    await bot.send_message(chat_id=chat_id, text=mensagem, disable_notification=True)
+        # Envia a mensagem para o chat especificado sem notificação
+        await bot.send_message(chat_id=chat_id, text=mensagem, disable_notification=True)
+    except Exception as e:
+        # Registra o erro e a mensagem
+        print(f"Erro ao enviar mensagem: {e}")
+
 
 
 def monta_mensagem_silenciosa(mensagem):
