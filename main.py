dda051e0f0fd76df0901563072e5e4b6c7db4a46
Luís Gustavo de Almeida <luis.gustavo@engenharia.ufjf.br>
2024-03-14 18:58:23 -0300

    atualizaçoes referente ao recolher automatico

--- main.py
+++ main.py
@@ -263,7 +263,6 @@ def roletas():
     # level_conta, valor_fichas_perfil = Mesa.dia_de_jogar_mesa(x_origem, y_origem, roleta, level_conta, valor_fichas_perfil, conta_upada,dia_da_semana)
     return
 
-
 def tarefas():
     print('Entrou nas tarefas')
     global x_origem, y_origem, roleta, hora_que_rodou, entrou_corretamente, stataus_facebook, pontuacao_tarefas
