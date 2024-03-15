813dbdb45929e7574c239ef44f3e690ad5e1b0db
Luís Gustavo de Almeida <luis.gustavo@engenharia.ufjf.br>
2024-03-15 17:44:16 -0300

    Implementação da comunicação recolhendo pelo firebase

--- BotPK.bat
+++ BotPK.bat
@@ -13,7 +13,6 @@ echo   **                                                          **
 echo   **************************************************************
 :inicio
 echo.
-timeout /t 0.3 /nobreak >nul
 REM Opções
 echo   **                 O que voce deseja fazer?                 **
 echo.
@@ -26,7 +25,6 @@ echo        6. Atualizar repositorio local com GitHub.
 echo.
 REM Define o tempo de espera em segundos
 set "tempo_espera=15"
-timeout /t 0.3 /nobreak >nul
 REM Pergunta ao usuário
 echo    Aguardando resposta.
 echo.
