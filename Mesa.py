9bc1a5e9611a439de6e604c7850e414b79e632e8
Luís Gustavo de Almeida <luis.gustavo@engenharia.ufjf.br>
2024-03-16 18:40:24 -0300

    Implementação da comunicação recolhendo pelo firebase

--- Mesa.py
+++ Mesa.py
@@ -1198,7 +1198,7 @@ def mesa_upar_jogar(x_origem, y_origem, numero_jogadas=3, upar=False, blind_mesa
 
                     lugares_ocupados = contar_pessoas_mesa(num_mesa)
                     print('firebase mesa com lugares ocupadas:', lugares_ocupados)
-                    if lugares_ocupados == 5:
+                    if lugares_ocupados >= 5:
                         mesa_completa = True
                         time_encher_mesa = time_entrou = time.perf_counter()
                     elif lugares_ocupados == 4:
