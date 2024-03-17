755d6aedd34ff230d44088619cbde7338ae99b1f
Luís Gustavo de Almeida <luis.gustavo@engenharia.ufjf.br>
2024-03-17 15:18:46 -0300

    Implementação da comunicação recolhendo pelo firebase

--- InicializarRecolherAuto.py
+++ InicializarRecolherAuto.py
@@ -1,6 +1,6 @@
 from Firebase import atualizar_configuracao_pc
 
-novos_dados = {'confg_funcao': 'Recolher_automatico', 'blind_recolher_auto': '200400', 'config_tempo_roleta': '4:40:5'}
+novos_dados = {'confg_funcao': 'Recolher_automatico', 'blind_recolher_auto': '1K2K', 'config_tempo_roleta': '4:40:5'}
 atualizar_configuracao_pc(novos_dados)
 
 print('\nBanco atualizado com sucesso!\n')
