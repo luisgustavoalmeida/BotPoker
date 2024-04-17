f9e5c828bdc4963c8dec93093efe18e7079c5431
Luís Gustavo de Almeida <luis.gustavo@engenharia.ufjf.br>
2024-04-16 23:33:54 -0300

    Implementação da comunicação recolhendo pelo firebase

--- Sub_processo.py
+++ Sub_processo.py
@@ -2,11 +2,14 @@ import os
 
 
 def inicializa_cmd_novo():
-    shortcut_path = r'Fechar_CMD.lnk'
+    arquivo_bat = r'Fechar_CMD.bat'  # Substitua pelo caminho do seu arquivo
 
     try:
-        os.startfile(shortcut_path)
+        os.system(arquivo_bat)  # Utilize o os.system para executar o arquivo .bat diretamente
     except Exception as e:
         print(f"Erro ao executar o arquivo .bat: {e}")
     else:
         print("Arquivo .bat executado com sucesso!")
+
+
+inicializa_cmd_novo()  # Chame a função para executar o arquivo .bat
