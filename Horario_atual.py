f65ac6cabd472978f81a96342fd7a215cc461e07
Luís Gustavo de Almeida <luis.gustavo@engenharia.ufjf.br>
2024-08-02 11:48:12 -0300

    Italia

--- Horario_atual.py
+++ Horario_atual.py
@@ -13,10 +13,14 @@ fuso_horario = pytz.timezone('Europe/Rome')
 # Converta a hora atual para o fuso horário desejado
 hora = hora_atual_local.astimezone(fuso_horario)
 
+# Formate a hora sem incluir o fuso horário
+hora_formatada = hora.strftime('%Y-%m-%d %H:%M:%S.%f')
+
 print("Hora atual no fuso horário local:", hora_atual_local)
-print("Hora atual no fuso horário desejado:", hora)
+print("Hora atual no fuso horário desejado:", hora_formatada)
 
 
 def horario():
     hora_atual = hora_atual_local.astimezone(fuso_horario)
+    hora_atual = hora_atual.strftime('%Y-%m-%d %H:%M:%S.%f')
     return hora_atual
