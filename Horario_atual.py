723b33db11a7cb5b1b38f6d57e9c5e6c333ecf5d
Luís Gustavo de Almeida <luis.gustavo@engenharia.ufjf.br>
2024-08-16 15:32:05 -0300

    Italia

--- Horario_atual.py
+++ Horario_atual.py
@@ -3,25 +3,25 @@ import time
 
 import pytz
 
-# Hora atual no fuso horário local
-hora_atual_local = datetime.datetime.now()
-
-# Defina o fuso horário desejado (por exemplo, 'America/Sao_Paulo')
-# fuso_horario = pytz.timezone('America/Sao_Paulo')
-
-# Defina o fuso horário da Itália
-fuso_horario = pytz.timezone('Europe/Rome')
-
-# Converta a hora atual para o fuso horário desejado
-hora = hora_atual_local.astimezone(fuso_horario)
-
-# Remover o fuso horário para exibir apenas a data e hora
-hora_sem_fuso = hora.replace(tzinfo=None)
-
-print("Hora atual no fuso horário local:", hora_atual_local)
-print("Hora atual no fuso horário desejado:", hora_sem_fuso)
-
-print("Dia da semana na Itália:", int(hora.weekday()))
+# # Hora atual no fuso horário local
+# hora_atual_local = datetime.datetime.now()
+#
+# # Defina o fuso horário desejado (por exemplo, 'America/Sao_Paulo')
+# # fuso_horario = pytz.timezone('America/Sao_Paulo')
+#
+# # Defina o fuso horário da Itália
+# fuso_horario = pytz.timezone('Europe/Rome')
+#
+# # Converta a hora atual para o fuso horário desejado
+# hora = hora_atual_local.astimezone(fuso_horario)
+#
+# # Remover o fuso horário para exibir apenas a data e hora
+# hora_sem_fuso = hora.replace(tzinfo=None)
+#
+# print("Hora atual no fuso horário local:", hora_atual_local)
+# print("Hora atual no fuso horário desejado:", hora_sem_fuso)
+#
+# print("Dia da semana na Itália:", int(hora.weekday()))
 
 
 def horario():
