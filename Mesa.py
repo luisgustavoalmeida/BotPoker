296ea0630bdb17b59f934299fd55bdd685446b36
Luís Gustavo de Almeida <luis.gustavo@engenharia.ufjf.br>
2024-10-17 18:25:40 -0300

    Italia

--- Mesa.py
+++ Mesa.py
@@ -1950,9 +1950,9 @@ def mesa_upar_jogar_recolher_slote(x_origem, y_origem, funcoes='', apostar=False
                     print('Fim do horario destinado a tarefas')
                     break
                 valor_fichas_ = OCR_tela.valor_fichas(x_origem, y_origem)
-                if valor_fichas_ < 5000:
+                if valor_fichas_ < 1000:
                     valor_fichas_ = OCR_tela.valor_fichas(x_origem, y_origem)
-                    if valor_fichas_ < 5000:
+                    if valor_fichas_ < 1000:
                         print('Quantidade de fichas baixa')
                         break
 
